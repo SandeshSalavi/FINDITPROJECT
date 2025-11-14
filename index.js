1ea492682a40ae51be30106f2da5b334d1cad9f0
Sandesh Salavi <sandeshsalavi3@gmail.com>
2025-11-14 20:59:29 +0530

    try to fix the table

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 // ================= ROUTES =================
 
 // Home
-app.get("/", isLoggedIn, (req, res) => {
+app.get("/",  (req, res) => {
     res.render("admin/home.ejs");
 });
 
